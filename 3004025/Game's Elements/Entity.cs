025e27f22885966fed82971c106d8e1b47fb0072
Leyson Dark <artemleykand@outlook.com>
2022-05-29 07:22:36 +0500

    Начал делать систему энтити

---
+++ 3004025/Game's Elements/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3004025
+{
+    public abstract class Entity
+    {
+        public abstract string Id { get; }
+        public abstract string Name { get; }
+
+        public virtual void Tick()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- 3004025/Game's Elements/Thing.cs
+++ 3004025/Game's Elements/Thing.cs
@@ -1,47 +1,11 @@
-﻿namespace _3004025
+﻿using System;
+
+namespace _3004025
 {
-    class Thing
+    public class Thing : Entity
     {
-        public Thing(string Name, ThingCategory Category, bool UseHitPoints, int HitPoints)
-        {
-            if (!UseHitPoints)
-                ErrorCather.ErrorMessage($"Объект {Name} имеет значение HitPoints, но UseHitPoints = false");
+        public override string Id => throw new NotImplementedException();
 
-            name = Name;
-            category = Category;    
-            useHitPoints = UseHitPoints;
-            hitPoints = HitPoints;
-        }
-        public Thing(string Name, ThingCategory Category, int HitPoints)
-        {
-            name = Name;
-            category = Category;
-            hitPoints = HitPoints;
-        }
-        public Thing(string Name, ThingCategory Category, bool UseHitPoints)
-        {
-            if (UseHitPoints)
-                ErrorCather.ErrorMessage($"Данная реализация объекта {Name} не поддерживает значение UseHitPoints = true");               
-
-            name = Name;
-            category = Category;
-            useHitPoints = UseHitPoints;
-        }
-
-        public string name = "";
-
-        public ThingCategory category;
-
-        public bool useHitPoints = true;
-
-        public int hitPoints = -1;
-
-        public void GetInfo()
-        {
-            Console.WriteLine("Name -> " + name);
-            Console.WriteLine("Category -> " + category);
-            Console.WriteLine("UseHitPoints is " + useHitPoints);
-            Console.WriteLine("HitPoints = " + hitPoints);
-        } 
+        public override string Name => throw new NotImplementedException();
     }
 }
